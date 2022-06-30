ca306892a0d5e10630ab7839efea56715e6660f0
Per Jakobsen <pj@pointwork.com>
2022-06-30 13:27:11 +0200

    Added upload.ps1

---
+++ upload.ps1
@@ -0,0 +1,103 @@
+Param(
+    [Parameter(Mandatory=$true,
+    ValueFromPipeline=$true)]
+    [string]
+    $myHost,
+
+    [Parameter(Mandatory=$true,
+    ValueFromPipeline=$true)]
+    [string]
+    $domain,
+
+    [Parameter(Mandatory=$true,
+    ValueFromPipeline=$true)]
+    [string[]]
+    $users
+)
+
+function Connect($user) {
+    $webUrl = "https://${myHost}/personal/${user}_$($domain -replace '\.','_')"
+    Write-Host "Processing $webUrl"
+    Connect-PnPOnline -Url $webUrl -CertificatePath .\moca365.pfx -ClientId 1fab0514-3f69-4133-8eaf-40db43364ca0 -Tenant $domain
+}
+
+###########################################################################
+# Inspired by
+#Author: Adnan Amin
+#blog: Https://mstechtalk.com
+#Downloading files from OneDrive to local machine
+###########################################################################
+function ProcessFolder($folderUrl, $sourceFolder, $isOneNote) {
+    $folder = Get-PnPFolder -RelativeUrl $folderUrl
+
+    if (-not (Test-Path -path $sourceFolder)) {
+        return
+    }
+    $existingFiles = Get-PnPProperty -ClientObject $folder -Property Files
+    Get-ChildItem $sourceFolder -File | ForEach-Object {
+        $file = $_
+        if (-not($existingFiles | Where-Object -Property Name -eq $file.Name))
+        {
+            Write-Host "Copying file " $file.Name " at " $folder.ServerRelativeUrl
+            [void](Add-PnPFile -Path $file.FullName -Folder $folder)
+        }
+    }
+    if ($isOneNote) {
+        $item = Get-PnPProperty -ClientObject $folder -Property ListItemAllFields
+        [void](Get-PnPProperty -ClientObject $item -Property parentList)
+        [void](set-pnplistitem -list $item.parentList -identity $item -values @{"HTML_x0020_File_x0020_Type"="OneNote.Notebook"})
+    }
+}
+
+function EnsureSubFolders($parentFolder, $currentPath, $subFolderNames) {
+    $subFolderNames | ForEach-Object {
+        $subFolderName = $_
+        if ($subFolderName) {
+            if (-not ($parentFolder.Folders | Where-Object -Property Name -eq $subFolderName)) {
+                $parent = $parentFolder.ServerRelativeUrl
+                Write-Host "Creating $subFolderName in $parent"
+                [void](Add-PnPFolder -Folder $parentFolder -Name $subFolderName)
+            }
+        }
+    }    
+}
+function ProcessSubFolders($folder, $folders, $currentPath, $onenotes) {
+    foreach ($folder in $folders) {
+        [void](Get-PnPProperty -ClientObject $folder -Property ServerRelativeUrl)
+        #Avoid Forms folders
+        if ($folder.Name -ne "Forms") {
+            write-host "Processing folder: ${folder.Name} .. at $currentPath"
+            $sourceFolder = Join-Path $currentPath $folder.Name;
+            ProcessFolder $folder.ServerRelativeUrl.Substring($web.ServerRelativeUrl.Length) $sourceFolder $onenotes.Contains($sourceFolder)
+            $tempfolders = Get-PnPProperty -ClientObject $folder -Property Folders
+            $subFolderNames = Get-ChildItem $sourceFolder -Directory | Select-Object -ExpandProperty Name
+            EnsureSubFolders $folder $currentPath $subFolderNames
+            $tempfolders = Get-PnPProperty -ClientObject $folder -Property Folders
+            ProcessSubFolders $web $tempfolders $sourceFolder $onenotes
+        }
+    }
+}
+
+function Upload($user) {
+    Connect $user
+    $web = Get-PnPWeb -ErrorAction Ignore
+    if (-not $web) {
+        Write-Error "Could not get site for $user"
+        return
+    }
+    if (-not (Test-Path $user -PathType container)) {
+        Write-Error "No folder for $user"
+        return
+    }
+    $list = Get-PNPList -Identity "Documents"
+    $onenotes = Get-Content (Join-Path $user 'OneNotes.txt') -ErrorAction Ignore
+    if (-not $onenotes) {
+        $onenotes = @{}
+    }
+    ProcessSubFolders $web $list.RootFolder $user $onenotes
+
+}
+
+$users | ForEach-Object {
+    Upload $_
+}
